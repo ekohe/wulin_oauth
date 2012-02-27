c96d7c1fcb429dcbb3e26f6cf2ac6dd0d4dbe0b0
Maxime Guilbot <maxime@ekohe.com>
2012-02-27 12:17:47 +0800

    MISC - Rails 3.2.1 compatibility & add current_user to abstract controller so that it's found in Metal

--- lib/application_controller.rb
+++ lib/application_controller.rb
@@ -9,51 +9,50 @@ module WulinOAuth
       end
     end
       
-    module InstanceMethods
-      def current_user
-        @current_user ||= User.from_session(session)
+    # InstanceMethods
+    def current_user
+      @current_user ||= User.from_session(session)
+    end
+    
+    def current_user=(new_user)
+      if new_user.nil?
+        session[:user] = nil
+      else
+        session[:user] = new_user.to_hash
       end
-      
-      def current_user=(new_user)
-        if new_user.nil?
-          session[:user] = nil
-        else
-          session[:user] = new_user.to_hash
-        end
-      end
+    end
 
-      def on_login_page?
-        params[:controller] == "wulin_oauth/user_sessions" && ["new", "create"].include?(params[:action])
-      end
+    def on_login_page?
+      params[:controller] == "wulin_oauth/user_sessions" && ["new", "create"].include?(params[:action])
+    end
 
-      def require_login
-        if current_user.nil?
-          return unauthorized_response
-        else
-          logger.warn "Authenticated as #{current_user.email}, ##{current_user.id}, #{current_user.level.inspect}"
-        end
+    def require_login
+      if current_user.nil?
+        return unauthorized_response
+      else
+        logger.warn "Authenticated as #{current_user.email}, ##{current_user.id}, #{current_user.level.inspect}"
       end
-      
-      def require_admin
-        if current_user.nil? || !current_user.admin?
-          return unauthorized_response
-        else
-          logger.warn "Authenticated as #{current_user.email}, ##{current_user.id}, #{current_user.level.inspect}"
-        end
+    end
+    
+    def require_admin
+      if current_user.nil? || !current_user.admin?
+        return unauthorized_response
+      else
+        logger.warn "Authenticated as #{current_user.email}, ##{current_user.id}, #{current_user.level.inspect}"
       end
+    end
 
-      def unauthorized_response
-        respond_to do |format|
-          format.html do
-            flash[:notice] = "You must be logged in to access this page."
-            redirect_to login_path
-          end
-          format.json { render :json => {:error => :not_authorized} }
+    def unauthorized_response
+      respond_to do |format|
+        format.html do
+          flash[:notice] = "You must be logged in to access this page."
+          redirect_to login_path
         end
-        false
+        format.json { render :json => {:error => :not_authorized} }
       end
+      false
     end
   end
 end
 
-::ActionController::Base.send :include, WulinOAuth::Controller
+::AbstractController::Base.send :include, WulinOAuth::Controller
