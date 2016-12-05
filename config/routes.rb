9130530938e6d2aafad9823043e8d4535820f12b
Ben Zheng <ben@ekohe.com>
2016-12-05 13:11:37 -0800

    MISC #59926 Upgrade MRLDB
    - Change Rails::VERSION::MAJOR to 5
    - Use get/post instead of match for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  match 'login' => "wulin_oauth/user_sessions#new", :as => :login, :via => :get
-  match 'login' => "wulin_oauth/user_sessions#create", :as => :login, :via => :post
-  match 'logout' => "wulin_oauth/user_sessions#destroy", :as => :logout
-  match 'oauth/callback' => "wulin_oauth/user_sessions#callback", :as => :callback, :via => :get
+  get 'login', to: 'wulin_oauth/user_sessions#new'
+  post 'login', to: 'wulin_oauth/user_sessions#create'
+  get 'logout', to: 'wulin_oauth/user_sessions#destroy'
+  get 'oauth/callback', to: 'wulin_oauth/user_sessions#callback', as: :callback
 end
