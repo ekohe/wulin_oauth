4881d067fb3666e7457e06c529383ed515f7278e
Torvalds Du <torvalds@ekohe.com>
2020-12-21 16:35:58 +0800

    MISC wulin_permits#6 - Remove unless settings for remove_user make it not global

--- app/grids/user_grid.rb
+++ app/grids/user_grid.rb
@@ -7,5 +7,5 @@ class UserGrid
   column :a_password, label: 'Has a password?', editable: false, formable: false, sortable: false, filterable: false
   column :welcome_email_sent_at, editable: false, formable: false
   action :add_user, screen: 'AddUserScreen', model: 'user', icon: :add_box, global: true, title: 'Invite User', only: [:MasterUserDetailRoleScreen]
-  action :remove_user, screen: 'AddUserScreen', model: 'user', icon: :clear, global: true, title: 'Remove User', only: [:MasterUserDetailRoleScreen]
+  action :remove_user, icon: :clear, title: 'Remove User', only: [:MasterUserDetailRoleScreen]
 end
