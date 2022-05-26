3cc810ed59c44ea3ff5b11795a1e81fd8b9050dd
mel <mel@ekohe.com>
2022-05-26 12:16:09 +0800

    https://gitlab.ekohe.com/ekohe/hbs/bss/-/issues/316 - Added a new api for fetching user info by user ids

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,47 @@
-require 'ostruct'
-require 'httparty'
+require "ostruct"
+require "httparty"
 
 class User
   extend ActiveModel::Naming
   class << self
+    def find_by_ids user_ids
+      query = {
+        oauth_token: current_user.access_token,
+        api_mode: true,
+        user_ids: user_ids.join(",")
+      }
+
+      url = "#{WulinOAuth.resource_host}/users.json?#{query.to_param}"
+
+      response = HTTParty.get url, timeout: 5
+
+      raise WulinOAuth::WulinOauthAuthenticationError if response.code == 401
+
+      json_text = response.body
+      users = ActiveSupport::JSON.decode(json_text)
+
+      users.map do |row|
+        User.new row
+      end
+    end
+
     # Creates a user from the code coming after the oauth login
     def get_access_token(code)
       return nil if code.nil? # Return nil if there's no code
 
-      response = ::HTTParty.post(WulinOAuth.access_token_url, :body => {
-        :client_id => WulinOAuth.oauth_identifier,
-        :client_secret => WulinOAuth.oauth_secret,
-        :redirect_uri => WulinOAuth.redirect_uri,
-        :code => code,
-        :grant_type => 'authorization_code'}
-      )
+      response = ::HTTParty.post(WulinOAuth.access_token_url, body: {
+        client_id: WulinOAuth.oauth_identifier,
+        client_secret: WulinOAuth.oauth_secret,
+        redirect_uri: WulinOAuth.redirect_uri,
+        code: code,
+        grant_type: "authorization_code"
+      })
 
       return nil if response["access_token"].nil? # Returns nil if there's no access token
 
       access_token = response["access_token"]
-      user_info = ActiveSupport::JSON.decode(HTTParty.get(WulinOAuth.resource_host + '/oauth/get_user/me.json', :query => {:oauth_token => access_token}).body)
-      new_user = self.new(HashWithIndifferentAccess.new(response.merge(user_info)))
+      user_info = ActiveSupport::JSON.decode(HTTParty.get(WulinOAuth.resource_host + "/oauth/get_user/me.json", query: {oauth_token: access_token}).body)
+      new_user = new(HashWithIndifferentAccess.new(response.merge(user_info)))
       return nil if new_user.id.nil? # Returns nil if there is no id associated to the user
 
       new_user
@@ -31,7 +52,7 @@ class User
       if session[:user].nil?
         nil
       else
-        self.new(session[:user])
+        new(session[:user])
       end
     end
 
@@ -52,28 +73,28 @@ class User
     end
 
     def column_names
-      %w(email)
+      %w[email]
     end
 
     def human_attribute_name(column)
       {
-        email: 'Email'
+        email: "Email"
       }[column.to_sym]
     end
 
     def find(id)
       if Array === id
-        all.select{|x| x.id.to_i == id.to_i}
+        all.select { |x| x.id.to_i == id.to_i }
       else
-        all.find{|x| x.id.to_i == id.to_i}
+        all.find { |x| x.id.to_i == id.to_i }
       end
     end
 
     def columns
-      [OpenStruct.new({:name => :email})]
+      [OpenStruct.new({name: :email})]
     end
 
-    %w(order limit offset includes joins where).each do |method_name|
+    %w[order limit offset includes joins where].each do |method_name|
       class_eval <<-RUBY, __FILE__, __LINE__ + 1
         def #{method_name}(*args)
           self
@@ -82,7 +103,7 @@ class User
     end
 
     # #map flatten uniq join
-    %w(map uniq flatten join).each do |method_name|
+    %w[map uniq flatten join].each do |method_name|
       class_eval <<-RUBY, __FILE__, __LINE__ + 1
         def #{method_name}(*args)
           all
@@ -105,15 +126,16 @@ class User
     end
 
     def all
-      self.to_a
+      to_a
     end
 
     alias_method :scoped, :all
 
-    def to_a(options = { invited: true })
+    def to_a(options = {invited: true})
       return [] unless current_user && @request_uri
-      invited_condition = options[:invited] ? '&invited_users_only=true' : '&uninvited_users_only=true&count=20000'
-      url = "#{WulinOAuth.resource_host}/users.json?#{@request_uri[12..-1]}#{invited_condition}&oauth_token=#{current_user.access_token}"
+      invited_condition = options[:invited] ? "&invited_users_only=true" : "&uninvited_users_only=true&count=20000"
+      query = URI(@request_uri).query
+      url = "#{WulinOAuth.resource_host}/users.json?#{query}#{invited_condition}&oauth_token=#{current_user.access_token}"
       response = HTTParty.get(url)
       raise WulinOAuth::WulinOauthAuthenticationError if response.code == 401
 
@@ -127,9 +149,7 @@ class User
       end
     end
 
-    def count
-      @count
-    end
+    attr_reader :count
 
     def invite(user_ids)
       url = "#{WulinOAuth.resource_host}/invitations"
@@ -163,17 +183,16 @@ class User
     def send_mail(user_id)
       url = "#{WulinOAuth.resource_host}/users/#{user_id}/send_mail_from_app"
       json_text = HTTParty.put(url, body: {
-        oauth_token: User.current_user.access_token,
+        oauth_token: User.current_user.access_token
       }).body
 
       ActiveSupport::JSON.decode(json_text)
     end
   end
 
+  attr_accessor :id, :ip, :email, :access_token, :refresh_token, :level, :expire_at, :app_admin, :a_password, :welcome_email_sent_at
 
-  attr_accessor :id, :ip, :email, :access_token, :refresh_token, :level, :expire_at, :expire_at, :app_admin, :a_password, :welcome_email_sent_at
-
-  def initialize(attributes={})
+  def initialize(attributes = {})
     attributes.symbolize_keys! if attributes.respond_to?(:symbolize_keys!)
     self.id = attributes[:id]
     self.ip = attributes[:ip]
@@ -190,21 +209,21 @@ class User
 
   def to_hash
     {
-      id: self.id,
-      ip: self.ip,
-      email: self.email,
-      access_token: self.access_token,
-      refresh_token: self.refresh_token,
-      expire_at: self.expire_at,
-      app_admin: self.app_admin,
-      a_password: self.a_password,
-      welcome_email_sent_at: self.welcome_email_sent_at,
-      level: self.level
+      id: id,
+      ip: ip,
+      email: email,
+      access_token: access_token,
+      refresh_token: refresh_token,
+      expire_at: expire_at,
+      app_admin: app_admin,
+      a_password: a_password,
+      welcome_email_sent_at: welcome_email_sent_at,
+      level: level
     }
   end
 
   def admin?
-    self.level == :administrator || app_admin?
+    level == :administrator || app_admin?
   end
 
   def app_admin?
