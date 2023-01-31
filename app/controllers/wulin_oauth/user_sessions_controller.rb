01ae5d8f56105c98537f763948b9d697e96759a1
Sarah Wang <sarah@ekohe.com>
2023-01-31 09:33:09 +0000

    FEATURE - ekohe/hbs/mima#31 remove change_password link from WulinMaster::AppBarMenu if current_user is a...

--- app/controllers/wulin_oauth/user_sessions_controller.rb
+++ app/controllers/wulin_oauth/user_sessions_controller.rb
@@ -40,11 +40,21 @@ class WulinOauth::UserSessionsController < ApplicationController
   def callback
     if params[:error].blank?
       self.current_user = User.get_access_token(params[:code])
+      handle_change_password_link
       redirect_to '/'
     else
       @new_authorization_url = WulinOAuth.new_authorization_url(:reset_session => true)
       render 'not_authorized'
       return
+    end
+  end
+
+  def handle_change_password_link
+    if current_user.can_change_password?
+      password_link = WulinMaster::AppBarMenu.menus.find(:user_menu).menus.detect {|menu| menu.name == :change_password }
+      WulinMaster::AppBarMenu.menus.find(:user_menu).add_menu(:change_password, label: 'Change Password', icon: :lock, order: 1, url: -> { WulinOAuth.change_password_uri }, target: :_blank) if password_link.nil?
+    else
+      WulinMaster::AppBarMenu.menus.find(:user_menu).menus.select! { |menu| menu.name != :change_password }
     end
   end
 end
