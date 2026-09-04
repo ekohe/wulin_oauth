729c60d8ab5c8d0b72e8519d5268c40a8c8ceb02
Mel Cao <mel@ekohe.com>
2026-09-04 11:42:02 +0800

    Add Propshaft asset path registration with Sprockets fallback

--- lib/wulin_oauth/engine.rb
+++ lib/wulin_oauth/engine.rb
@@ -5,7 +5,11 @@ require 'action_controller'
 module WulinOAuth
   class Engine < ::Rails::Engine
     initializer "add assets to precompile" do |app|
-       app.config.assets.precompile += %w( wulin_oauth.js wulin_oauth.css indicator.gif )
+      if defined?(Propshaft)
+        app.config.assets.paths << root.join("app", "assets", "images")
+      else
+        app.config.assets.precompile += %w( wulin_oauth.js wulin_oauth.css indicator.gif )
+      end
     end
   end
 end
